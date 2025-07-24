80b26d781b232910723d28430613b0614a99e23f
MariamMohi <mariam.mohiuddin@torontomu.ca>
2025-07-24 03:34:13 -0400

    our website upload

---
+++ about-script.js
@@ -0,0 +1,9 @@
+const joinBtn = document.getElementById("join-btn");
+
+joinBtn.addEventListener("mouseover", function() {
+  console.log("Mouse is over the button");
+});
+
+joinBtn.addEventListener("mouseout", function() {
+  console.log("Mouse left the button");
+});
